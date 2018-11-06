ad38d8c31774913b1e18cc50ce443b43fa466672
s-a-p <josh.furlin@gmail.com>
2018-11-05 20:19:48 -0600

    created react project with routing structure

---
+++ portfoolio_dx/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Bio from "./pages/Bio";
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Bio} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ portfoolio_dx/src/pages/Bio/Bio.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Books extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>my name is josh and i'm here to say</h1>
+        <h1>i want to go outside and play</h1>
+      </div>
+    );
+  }
+}
+
+export default Books;
