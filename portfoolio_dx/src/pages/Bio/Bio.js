2f1a9cf8d3fceb147b6086459020ae24b961791b
s-a-p <josh.furlin@gmail.com>
2018-11-07 20:34:07 -0600

    added links for projects

--- portfoolio_dx/src/pages/Bio/Bio.js
+++ portfoolio_dx/src/pages/Bio/Bio.js
@@ -6,8 +6,93 @@ class Books extends Component {
   render() {
     return (
       <div>
-        <h1>my name is josh and i'm here to say</h1>
-        <h1>i want to go outside and play</h1>
+        <div>
+          <h1>Wild West Hangman</h1>
+          <a href="https://github.com/space-age-pete/Word-Guess-Game">Github Repo</a>
+          <br/>
+          <a href="https://space-age-pete.github.io/Word-Guess-Game/">Github Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+  
+        <div>
+          <h1>Melee Battle Simulator</h1>
+          <a href="https://github.com/space-age-pete/unit-4-game">Github Repo</a>
+          <br/>
+          <a href="https://space-age-pete.github.io/unit-4-game/">Github Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+  
+        <div>
+          <h1>Trivia Game</h1>
+          <a href="https://github.com/space-age-pete/TriviaGame">Github Repo</a>
+          <br/>
+          <a href="https://space-age-pete.github.io/TriviaGame/">Github Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Train Scheduler</h1>
+          <a href="https://github.com/space-age-pete/Train-Scheduler">Github Repo</a>
+          <br/>
+          <a href="https://space-age-pete.github.io/Train-Scheduler/">Github Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+         <div>
+          <h1>Liribot</h1>
+          <a href="https://github.com/space-age-pete/liri-node-app">Github Repo</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Back End Hangman</h1>
+          <a href="https://github.com/space-age-pete/constructor-word-guess">Github Repo</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Clamazon</h1>
+          <a href="https://github.com/space-age-pete/clamazon">Github Repo</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Burger Eater</h1>
+          <a href="https://github.com/space-age-pete/burger">Github Repo</a>
+          <br/>
+          <a href="https://dashboard.heroku.com/apps/infinite-sands-16651">Heroku Repo</a>
+          <br/>
+          <a href="https://infinite-sands-16651.herokuapp.com/">Heroku Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Jacobin Mongrio</h1>
+          <a href="https://github.com/space-age-pete/mongrio">Github Repo</a>
+          <br/>
+          <a href="https://dashboard.heroku.com/apps/hidden-brushlands-93967">Heroku Repo</a>
+          <br/>
+          <a href="https://hidden-brushlands-93967.herokuapp.com/">Heroku Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
+
+        <div>
+          <h1>Clicky Game</h1>
+          <a href="https://github.com/space-age-pete/clicky-game">Github Repo</a>
+          <br/>
+          <a href="https://space-age-pete.github.io/clicky-game/">Github Deployed</a>
+          <h3>Notes:</h3>
+          <p></p>
+        </div>
       </div>
     );
   }
